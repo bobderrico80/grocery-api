f14c31b2ef30a150e982370e6828a97cb78adee4
Bob D'Errico <bob@bobderrico.com>
2018-07-07 16:14:44 -0400

    Add postgres and implement postgres check in healthcheck

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
+const Sequelize = require('sequelize');
 const packageJson = require('./package.json');
 const logger = require('./lib/logger');
 
 const port = process.env.PORT || 8080;
 
 const app = express();
+const sequelize = new Sequelize(process.env.POSTGRES_CONNECTION_URL, {
+  logging: false,
+  operatorsAliases: false,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const router = express.Router();
 
-router.get('/healthcheck', (req, res) => {
-  res.status(200).send();
+router.get('/healthcheck', async (req, res) => {
+  try {
+    await sequelize.authenticate();
+    res.status(200).send();
+  } catch (error) {
+    res.status(503).send();
+  }
 });
 
 router.get('/version', (req, res) => {
