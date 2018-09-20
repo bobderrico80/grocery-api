1112991ff2f1018bed40c363bbf36bb9b5ac85d7
Bob D'Errico <bob@bobderrico.com>
2018-09-20 15:06:37 -0400

    Add encrypted password field to User model

--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,8 @@
 const RestController = require('../lib/RestController');
+const removeFields = require('../lib/removeFields');
 const User = require('../model/User');
 
-module.exports = RestController(User);
+module.exports = RestController(User, {
+  // Remove password fields from responses.
+  responseDataFilter: (req, res, data) => removeFields(data, ['password']),
+});
--- model/User.js
+++ model/User.js
@@ -1,13 +1,27 @@
+const bcrypt = require('bcrypt');
 const { Sequelize, sequelize } = require('../lib/db');
 
-const User = sequelize.define('user', {
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    unique: true,
-    validate: { isEmail: true },
+const User = sequelize.define(
+  'user',
+  {
+    email: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+      validate: { isEmail: true },
+    },
+    name: { type: Sequelize.STRING, allowNull: false },
+    password: { type: Sequelize.STRING, allowNull: false },
   },
-  name: { type: Sequelize.STRING, allowNull: false },
-});
+  {
+    hooks: {
+      beforeCreate: async (user) => {
+        const hashedPassword = await bcrypt.hash(user.password, 12);
+        // eslint-disable-next-line no-param-reassign
+        user.password = hashedPassword;
+      },
+    },
+  },
+);
 
 module.exports = User;
